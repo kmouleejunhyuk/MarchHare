0f232e13fa02027f6d7b8a442d46fd1b5cea596e
junhyeok <ksi03376@naver.com>
2022-10-17 01:15:17 +0900

    modify airflow scripts

--- examples/airflow/settings/airflow/set_master.sh
+++ examples/airflow/settings/airflow/set_master.sh
@@ -1,13 +1,13 @@
-export AIRFLOW_NAME="airflow-cluster"
-export AIRFLOW_NAMESPACE="airflow-cluster"
 
-## create the namespace
-kubectl create ns "$AIRFLOW_NAMESPACE"
+#you need cluster named default
+export AIRFLOW_NAME="airflow-cluster"
+export AIRFLOW_NAMESPACE="default"
 
 ## install using helm 3
 helm install \
   "$AIRFLOW_NAME" \
   airflow-stable/airflow \
   --namespace "$AIRFLOW_NAMESPACE" \
-  --version "8.X.X" \
-  --values ./custom-values.yaml
+  --values ./airflow_helm.yaml
+
+kubectl port-forward svc/${AIRFLOW_NAME}-web 8080:8080 --namespace $AIRFLOW_NAMESPACE
