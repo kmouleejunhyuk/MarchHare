f5c1ab3195bbc636252023d72b23ceb74b498171
kmouleejunhyuk <jh_lee@g.kmou.ac.kr>
2022-10-16 14:24:04 +0900

    add airflow & minikube example cases

---
+++ examples/airflow/settings/minikube/install_mac.sh
@@ -0,0 +1,23 @@
+#!/bin/zsh
+# install guide: https://kubernetes.io/docs/tasks/tools/install-kubectl-macos/
+# install as master node
+
+# requirements: Homebrew & zsh & sudo 
+    # add 
+    # autoload -Uz compinit; \n compinit
+    # to ~/.zshrc
+
+brew install kubectl
+
+source <(kubectl completion zsh)
+
+brew install minikube
+brew install --cask docker
+kubectl cluster-info dump
+
+# after this:
+    # run docker desktop(or run docker by systemctl)
+    # kubectl cluster-info
+    # add alias kubectl="minikube kubectl --"
+    # minikube start
+    # minikube webserver
