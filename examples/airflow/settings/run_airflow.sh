86d93a92cd47695bb825666c84e19b9dfdc2a874
kmouleejunhyuk <jh_lee@g.kmou.ac.kr>
2022-10-21 12:08:48 +0900

    modify network settings

--- examples/airflow/settings/run_airflow.sh
+++ examples/airflow/settings/run_airflow.sh
@@ -1,8 +1,9 @@
-minikube start --driver='docker' --profile='airflow-demo'  --cni='calico' --kubernetes-version='stable' --nodes 3 
+minikube start --driver='docker' --profile='airflow' --kubernetes-version='stable' --nodes 3
 helm install airflow airflow-stable/airflow --namespace default --values ./airflow_helm.yaml 
 
 # monitoring minikube
 # minikube dashboard -p profile --url -> opens url(8080)
-# kubectl proxy --address='0.0.0.0' --disable-filter=true -> opens proxy(8001)
-# in Local(labtop): ssh -L 12345:localhost:8001 user@remote-ip
-# connect to: http://localhost:12345/api/v1/namespaces/kubernetes-dashboard/services/http:kubernetes-dashboard:/proxy/#/pod?namespace=default
+# in Local(labtop): ssh -L 12345:localhost:8080 user@remote-ip -> airflow dashboard
+# in Local(labtop): ssh -L 11111:localhost:nnnn user@remote-ip -> minikube dashboard
+# connect to: http://localhost:11111/api/v1/namespaces/kubernetes-dashboard/services/http:kubernetes-dashboard:/proxy/#/pod?namespace=default
+# connect to: http://localhost:12345
